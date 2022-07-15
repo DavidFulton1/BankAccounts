1b2e6be3b3f9d58977117b636ca3c6794e02c1f7
DavidFulton1 <fultondavids@gmail.com>
2022-07-15 09:20:20 -0500

    first commit

---
+++ Bank_Encapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Encapsulation
+{
+    public class BankAccount
+    {public BankAccount()
+        { 
+        }
+        private static double balance = 0;
+        public static void Deposit( double x)
+        {
+            balance = x + balance;
+            Console.WriteLine($"Your new balance is {balance}");
+        }
+
+       
+    }
+}
---
+++ Bank_Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿using Bank_Encapsulation;
+
+
+var account1 = new BankAccount();
+Console.WriteLine("what is your deposit ammount?");
+var ammount = double.Parse(Console.ReadLine());
+
+BankAccount.Deposit(ammount);
+
